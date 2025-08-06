8884000c614cedab7c46b13c1ea69d5c8961719f
Nathaniel J. Liberty <nathaniel.liberty@gmail.com>
2025-08-06 19:36:46 -0400

    Remove extra whitespace from doc comments

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,16 @@
 //! # EOCLU
-//! 
+//!
 //! The library for EOCLU, a command line utility for EVE Online by CCP Games.
-//! 
+//!
 //! This crate is intended for use by the EOCLU binary.
 
 /// Runs EOCLU.
-/// 
+///
 /// This is the entrypoint for EOCLU and is intended to be called by `main` in
 /// the EOCLU binary crate.
-/// 
+///
 /// # Examples
-/// 
+///
 /// ```
 /// eoclu::run();
 /// ```
