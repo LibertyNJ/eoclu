f7b3c0747864f9e418a9a7d59adadd0e9d16b7cb
Nathaniel J. Liberty <nathaniel.liberty@gmail.com>
2025-08-06 19:25:45 -0400

    Document binary entrypoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+//! A command line utility for EVE Online by CCP Games.
+//!
+//! EOCLU seeks to offer a convenient command line interface for interacting
+//! with the Tranquility server cluster. This utility is designed to be
+//! approachable by users who are curious and may be new to this kind of thing,
+//! but also offer more advanced features for users who want to use it to
+//! compose a larger script or other program.
+
 fn main() {
     println!("Hello, world!");
 }
