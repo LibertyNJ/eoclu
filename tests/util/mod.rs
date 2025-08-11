4bb2fc0384f4ff1ae9290398e0b87042077ac9a4
Nathaniel J. Liberty <nathaniel.liberty@gmail.com>
2025-08-10 22:01:45 -0400

    Assert output snapshots

--- tests/util/mod.rs
+++ tests/util/mod.rs
@@ -3,3 +3,16 @@ use assert_cmd::Command;
 pub fn create_test_command() -> Command {
     Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap()
 }
+
+macro_rules! assert_output_snapshot {
+    ($output:expr) => {{
+        let snapshot = format!(
+            "stdout:\n{}\nstderr:\n{}",
+            String::from_utf8_lossy(&$output.stdout),
+            String::from_utf8_lossy(&$output.stderr)
+        );
+        insta::assert_snapshot!(snapshot);
+    }};
+}
+
+pub(crate) use assert_output_snapshot;
